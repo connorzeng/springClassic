0ce28a3c2010bb9725c0a1cd16a27abb38c8b3b8
Connor Zeng <ganggang334@gmail.com>
2021-08-30 13:58:48 +0800

    springmvc

--- src/main/java/com/connor/controller/ConnorController.java
+++ src/main/java/com/connor/controller/ConnorController.java
@@ -1,9 +1,15 @@
 package com.connor.controller;
 
 
+import com.connor.entity.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -11,11 +17,20 @@ public class ConnorController {
 
 
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello(){
+    @Autowired
+    private ApplicationContext applicationContext;
 
-        return "hello";
+    @Autowired(required = false)
+    private UserDTO userDTO;
+
+    @RequestMapping("/hello.do")
+    @ResponseBody
+    public Map<String,String> hello(){
+
+        HashMap<String, String> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("name","connor");
+
+        return objectObjectHashMap;
     }
 
 }
